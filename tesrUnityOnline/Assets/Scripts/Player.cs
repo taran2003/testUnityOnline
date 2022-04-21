d5482a961a1348b880b739de64be3b8262491333
taran2003 <taran2003@bk.ru>
2022-04-21 21:54:49 +0300

    jump function

--- tesrUnityOnline/Assets/Scripts/Player.cs
+++ tesrUnityOnline/Assets/Scripts/Player.cs
@@ -6,20 +6,62 @@ using Photon.Pun;
 public class Player : MonoBehaviour
 {
     public float speed;
+    private float jumpForce = 15f; 
+    public float radius; 
     PhotonView view;
+    SpriteRenderer sprite;
+    private Vector3 dir = new Vector3();
+    Rigidbody2D rb;
+    private bool isGrounded;
 
     void Start()
     {
         view = GetComponent<PhotonView>();
+        sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
     }
 
     void Update()
     {
         if (view.IsMine) 
         { 
-            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
-            transform.position += (Vector3)moveAmount;
+            if (Input.GetButton("Horizontal"))
+            {
+                Run();
+            }
+            if (isGrounded && Input.GetButton("Jump"))
+            {
+                Jump();
+            }
+            //Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            //Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
+            //transform.position += (Vector3)moveAmount;
         }
+        if(Input.GetButton("Horizontal"))
+        {
+            sprite.flipX = dir.x < 0.0f;
+        }
+    }
+
+    private void Run()
+    {
+        dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+        isGrounded = colliders.Length > 1;
     }
 }
