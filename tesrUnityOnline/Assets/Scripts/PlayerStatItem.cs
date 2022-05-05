61a31c5e48b64d2546f0b154864d522eea8db476
taran2003 <taran2003@bk.ru>
2022-05-05 21:35:24 +0300

    Add PlayerStats Ui

--- tesrUnityOnline/Assets/Scripts/PlayerStatItem.cs
+++ tesrUnityOnline/Assets/Scripts/PlayerStatItem.cs
@@ -1,21 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerStatItem : MonoBehaviour
 {
     public Photon.Realtime.Player player;
 
-    public Text nm, k, d;
+    public TMPro.TMP_Text nm, k, d;
 
     private void Start()
     {
         if (player.CustomProperties["K"] != null)
         {
             nm.text = player.NickName.Split('-')[0];
-            k.text = ((int)player.CustomProperties["K"]).ToString("0");
-            d.text = ((int)player.CustomProperties["D"]).ToString("0");
+            k.text = ((int)player.CustomProperties["K"]).ToString("00");
+            d.text = ((int)player.CustomProperties["D"]).ToString("00");
         }
         else
         {
